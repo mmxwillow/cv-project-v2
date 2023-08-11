a6654608284e2b7039e59150c830f5fb0eae9d15
mmxwillow <mxwierzba@gmail.com>
2023-08-11 10:16:11 +0200

    Logic for switching between views

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
+import Preview from './components/Preview'
+import Edit from './components/Edit'
 import './styles/App.css'
 
 function App() {
+  const [currentView, setCurrentView] = useState('Edit');
+
+  const changeView = () =>{
+    const newView = (currentView === 'Edit') ? 'Preview' : 'Edit';
+    setCurrentView(newView); 
+  }
+
   return (
     <>
       <h1>CV App</h1>
+      <button onClick={changeView}>{currentView}</button>
+      {(currentView === 'Edit') ? <Preview /> : <Edit /> }
     </>
   )
 }
