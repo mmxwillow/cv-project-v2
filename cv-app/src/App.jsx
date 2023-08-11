aa5e371a89064fe48862baf97144990c47fe8adb
mmxwillow <mxwierzba@gmail.com>
2023-08-11 10:38:46 +0200

    Added CV state

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -6,16 +6,30 @@ import './styles/App.css'
 function App() {
   const [currentView, setCurrentView] = useState('Edit');
 
+  const [cv, setCV] = useState({
+    general: {
+      name:'',
+      email:'',
+      phone:'',
+    },
+    education: [],
+    experience: []
+  })
+
   const changeView = () =>{
     const newView = (currentView === 'Edit') ? 'Preview' : 'Edit';
     setCurrentView(newView); 
   }
 
+
   return (
     <>
       <h1>CV App</h1>
       <button onClick={changeView}>{currentView}</button>
-      {(currentView === 'Edit') ? <Preview /> : <Edit /> }
+      <div id="cv">
+      {(currentView === 'Edit') ? <Preview cv={cv}/> : <Edit cv={cv}/> }
+      {console.log(cv)}
+      </div>
     </>
   )
 }
