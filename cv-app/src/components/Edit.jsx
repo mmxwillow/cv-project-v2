ccb4f4b37bccdb865b0cacaac808179213c5ddac
mmxwillow <mxwierzba@gmail.com>
2023-08-11 19:51:20 +0200

    Added functions as props

--- cv-app/src/components/Edit.jsx
+++ cv-app/src/components/Edit.jsx
@@ -2,12 +2,12 @@ import EditGeneral from "./EditGeneral";
 import EditEducation from "./EditEducation";
 import EditExperience from "./EditExperience";
 
-function Edit({cv}){
+function Edit({cv, updateGeneral, updateExperienceItem, deleteExperienceItem, addExperienceItem, updateEducationItem, deleteEducationItem, addEducationItem}){
     return (
         <>
-        <EditGeneral general={cv.general}/>
-        <EditEducation education={cv.education}/>
-        <EditExperience experience={cv.experience}/>
+        <EditGeneral general={cv.general} updateGeneral={updateGeneral}/>
+        <EditEducation education={cv.education} updateExperienceItem={updateExperienceItem} deleteExperienceItem={deleteExperienceItem} addExperienceItem={addExperienceItem} />
+        <EditExperience experience={cv.experience} updateEducationItem={updateEducationItem} deleteEducationItem={deleteEducationItem} addEducationItem={addEducationItem} />
         </>
     )
 }
