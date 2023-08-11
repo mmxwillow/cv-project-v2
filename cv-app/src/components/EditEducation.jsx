2ff6b8552a1662f071575419d7e45beabe9f1846
mmxwillow <mxwierzba@gmail.com>
2023-08-11 22:10:35 +0200

    Added key attribute

--- cv-app/src/components/EditEducation.jsx
+++ cv-app/src/components/EditEducation.jsx
@@ -5,9 +5,9 @@ function EditEducation({education, updateEducationItem, deleteEducationItem, add
             {
                 education.map((item) => {
                     return (
-                        <div key={item.id}>
-                            <input type="text" onChange={updateEducationItem} name="schoolName" placeholder="School name" value={item.name}/>
-                            <input type="text" onChange={updateEducationItem} name="studyTitle" placeholder="Study title" value={item.title}/>
+                        <div key={item.id} data-key={item.id}>
+                            <input type="text" onChange={updateEducationItem} name="name" placeholder="School name" value={item.name}/>
+                            <input type="text" onChange={updateEducationItem} name="title" placeholder="Study title" value={item.title}/>
                             <input type="text" onChange={updateEducationItem} name="date" placeholder="Date" value={item.date}/>
                             <button onClick={deleteEducationItem}>Delete section</button>
                         </div>
